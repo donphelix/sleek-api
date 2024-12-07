af1b6f1f0330d5580192276450ea2941cd8ae0cc
Don Phelix <141995407+dondevteam@users.noreply.github.com>
2024-12-07 22:30:28 +0300

    add api.php for a stateless API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,15 @@
 <?php
 
 use App\Http\Controllers\Api\PropertyController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
 
 Route::apiResource('properties', PropertyController::class);
 
+Route::get("/users", function () {
+    return "coming soon";
+});
